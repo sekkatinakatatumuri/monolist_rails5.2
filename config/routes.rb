b1c08540e4e1d54a05ac626c8f7448b5d9b982fc
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-08-27 05:21:51 +0000

    add: トップページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # URLがルートだった場合の飛ぶ先を変更
+  root to: 'toppages#index'
 end
