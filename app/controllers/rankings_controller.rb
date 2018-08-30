f5103b069d95edb6c3a7f4c040e30c1d1efb0fda
sekkatinakatatumuri <sekkatinakatatumuri@gmail.com>
2018-08-30 02:14:05 +0000

    add: Wantランキング

---
+++ app/controllers/rankings_controller.rb
@@ -0,0 +1,10 @@
+class RankingsController < ApplicationController
+  def want
+    # Wantモデルのrankingクラスメソッドの戻り値を格納
+    @ranking_counts = Want.ranking
+    
+    # @ranking_counts.keys でハッシュから item_id だけ取り出した配列を取得
+    # それらの items を @items = Item.find(@ranking_counts.keys) で items を取得
+    @items = Item.find(@ranking_counts.keys)
+  end
+end
--- app/models/want.rb
+++ app/models/want.rb
@@ -1,4 +1,13 @@
 class Want < Ownership
   # Want.all を実行すると、 SELECT ownerships.* FROM ownerships WHERE ownerships.type IN ('Want') という SQL が実行され、
-  # ownerships のテーブルの type IN ('Want') (type = 'Want' と同じ) なレコードのみを取得
+  # ownerships のテーブルの type IN ('Want') (type = 'Want' と同じ) なレコードのみを取得できる
+  
+  # wantのランキング返すクラスメソッド
+  def self.ranking
+    # .group(:item_id) → レコードを item_id カラムでグループ化
+    # .order('count_item_id DESC') → ant のカウントが多い順にソート
+    # .count(:item_id) → グループさせた item_id の数をカウント
+    # .limit(10) → ランキングに表示する数を10個に絞る
+    self.group(:item_id).order('count_item_id DESC').limit(10).count(:item_id)
+  end
 end
